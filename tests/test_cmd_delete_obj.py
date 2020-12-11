7433065c7376dea0e24bab111d5543acfef92331
Thobias Salazar Trevisan <thobiast@gmail.com>
2020-12-11 08:53:08 -0300

    Add cmd_delete_obj test

---
+++ tests/test_cmd_delete_obj.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Test cmd delete obj."""
+
+import pytest
+from unittest.mock import Mock, patch
+
+from s3_client import s3_client
+
+
+def test_cmd_delete_obj(s3):
+    """Test cmd_delete_obj function."""
+    args = Mock()
+    args.bucket = "my_bucket"
+    args.object = "my_object"
+    with patch.object(s3, "check_bucket_exist", return_value=True):
+        with patch.object(s3, "delete_object"):
+            s3_client.cmd_delete_obj(s3, args)
+            s3.delete_object.assert_called_with(args.bucket, args.object)
+
+
+def test_cmd_delete_obj_bucket_not_exist(s3):
+    """Test if bucket does not exist."""
+    s3_client.msg = Mock()
+    args = Mock()
+    args.bucket = "my_bucket"
+    args.object = "my_object"
+    with patch.object(s3, "check_bucket_exist", return_value=False):
+        with patch.object(s3, "delete_object"):
+            s3_client.cmd_delete_obj(s3, args)
+            s3_client.msg.assert_called_with(
+                "red", "Error: Bucket 'my_bucket' does not exist", 1
+            )
