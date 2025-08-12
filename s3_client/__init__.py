5d6dab15f47af006314bb2c3355212217a5052f0
Thobias Salazar Trevisan <thobiast@gmail.com>
2025-08-12 18:24:23 -0300

    Migrate project packaging from setup.py to pyproject.toml and update pre-commit

--- s3_client/__init__.py
+++ s3_client/__init__.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 """s3_client module."""
 
-__name__ = "s3_client"
-__description__ = "Sample python script to work with Amazon S3."
-__url__ = "https://github.com/thobiast/s3_client"
-__license__ = "MIT"
-__author__ = "Thobias Salazar Trevisan"
-
-
-from s3_client.__version__ import __version__  # noqa
+__version__ = "0.1.3"
 
 # vim: ts=4
